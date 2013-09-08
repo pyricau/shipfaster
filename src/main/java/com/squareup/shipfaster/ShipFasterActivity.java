3ca253d3cad89153d2dc41c2c40024ee6e819e1d
Pierre-Yves Ricau <py@squareup.com>
2013-09-08 23:02:28 +0200

    empty project

---
+++ src/main/java/com/squareup/shipfaster/ShipFasterActivity.java
@@ -0,0 +1,11 @@
+package com.squareup.shipfaster;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ShipFasterActivity extends Activity {
+
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+  }
+}
