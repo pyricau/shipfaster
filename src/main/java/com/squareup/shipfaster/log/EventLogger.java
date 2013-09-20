e804c83a91af54ee7b9a4ccddef58a920ea6894e
Pierre-Yves Ricau <py@squareup.com>
2013-09-20 09:36:48 +0200

    don't register twice

--- src/main/java/com/squareup/shipfaster/log/EventLogger.java
+++ src/main/java/com/squareup/shipfaster/log/EventLogger.java
@@ -1,18 +1,12 @@
 package com.squareup.shipfaster.log;
 
-import com.squareup.otto.Bus;
 import com.squareup.otto.Subscribe;
 import com.squareup.shipfaster.swipe.SwipeEvent;
 import javax.inject.Inject;
 
 public class EventLogger {
 
-  private final FileLogger fileLogger;
-
-  @Inject EventLogger(FileLogger fileLogger, Bus bus) {
-    this.fileLogger = fileLogger;
-    bus.register(this);
-  }
+  @Inject FileLogger fileLogger;
 
   @Subscribe public void logSwipe(SwipeEvent event) {
     fileLogger.add(new SwipeLog(event.successfulSwipe));
